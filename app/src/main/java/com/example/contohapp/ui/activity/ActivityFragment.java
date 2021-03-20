efb14394ff6dc3f0ff7a9c0c403940daa9af58b9
hollyyph <hollyana.puteri.haryono@gmail.com>
2021-03-21 00:13:09 +0700

    push files

---
+++ app/src/main/java/com/example/contohapp/ui/activity/ActivityFragment.java
@@ -0,0 +1,35 @@
+package com.example.contohapp.ui.activity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.contohapp.R;
+
+public class ActivityFragment extends Fragment {
+
+    private ActivityViewModel activityViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        activityViewModel =
+                new ViewModelProvider(this).get(ActivityViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_activity, container, false);
+        final TextView textView = root.findViewById(R.id.text_activity);
+        activityViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
